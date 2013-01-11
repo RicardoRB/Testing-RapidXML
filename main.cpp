b7d10cfe5a818b6651fa90d15cf11ee8a2ebf21d
Richy501 <soulfear91@gmail.com>
2013-01-11 13:27:42 +0100

    Refresh project

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 using namespace rapidxml;
 
 int main() {
-    //locale::global(locale("spanish"));
     ifstream myfile("english.xml");
     xml_document<> doc;
     /* "Read file into vector<char>"  See linked thread above*/
@@ -15,7 +14,7 @@ int main() {
 
     buffer.push_back('\0');
 
-    cout<<&buffer[0]<<endl; /*test the buffer */
+    cout << &buffer[0] << endl;
 
     doc.parse<0>(&buffer[0]);
 
@@ -24,12 +23,11 @@ int main() {
     xml_node<> *node = doc.first_node(doc.first_node()->name());
     cout << "Name first subnode of my first node is: " << node->first_node()->name() << endl;
     cout << "Node " << node->first_node()->name() << " has value " << node->first_node()->value() << endl;
-
+    cout << "List of all nodes" << endl;
     for (xml_node<> *child = node->first_node(); child; child = child->next_sibling()) {
         cout << child->name() << endl;
     }
-
-
+    cout << "List of all attributes of first node" << endl;
     for (xml_attribute<> *attr = node->first_node()->first_attribute(); attr; attr = attr->next_attribute()) {
         cout << "Node foobar has attribute " << attr->name() << " ";
         cout << "with value " << attr->value() << "\n";
