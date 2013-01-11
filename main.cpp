f8a12bba35926a33c0db7e3c8c2e804fa127c87e
Richy501 <soulfear91@gmail.com>
2013-01-11 13:01:56 +0100

    Nodes, attributes and iterator nodes

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <rapidxml.hpp>
+
+using namespace std;
+using namespace rapidxml;
+
+int main() {
+    //locale::global(locale("spanish"));
+    ifstream myfile("english.xml");
+    xml_document<> doc;
+    /* "Read file into vector<char>"  See linked thread above*/
+    vector<char> buffer((istreambuf_iterator<char>(myfile)), istreambuf_iterator<char>( ));
+
+    buffer.push_back('\0');
+
+    cout<<&buffer[0]<<endl; /*test the buffer */
+
+    doc.parse<0>(&buffer[0]);
+
+    cout << "Name of my first node is: " << doc.first_node()->name() << endl;
+
+    xml_node<> *node = doc.first_node(doc.first_node()->name());
+    cout << "Name first subnode of my first node is: " << node->first_node()->name() << endl;
+    cout << "Node " << node->first_node()->name() << " has value " << node->first_node()->value() << endl;
+
+    for (xml_node<> *child = node->first_node(); child; child = child->next_sibling()) {
+        cout << child->name() << endl;
+    }
+
+
+    for (xml_attribute<> *attr = node->first_node()->first_attribute(); attr; attr = attr->next_attribute()) {
+        cout << "Node foobar has attribute " << attr->name() << " ";
+        cout << "with value " << attr->value() << "\n";
+    }
+    return 0;
+}
